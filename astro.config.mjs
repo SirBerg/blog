f5240c85e4100b6c7916f4c0717166f6ecf3ebf5
SirBerg <benno@trellis.de>
2023-08-18 23:55:42 +0200

    Updated index image to WebP (apparently that has better compression)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://sirberg.tokyo',
 	integrations: [mdx(), sitemap()],
-	experimental: {
-		viewTransitions: true
-	}
 });
